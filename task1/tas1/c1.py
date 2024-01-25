a7a1daffe8e1fca3697d0d69d66833ef6e8fa3d4
parkjeonghyeon <pkys1020@naver.com>
2024-01-25 18:33:38 +0900

    remove in web and ignore check

--- task1/tas1/c1.py
+++ task1/tas1/c1.py
@@ -1,2 +1,43 @@
-print('hello')
-return 0
+import pprint
+
+json_data = {
+    'project': {
+        'name': 'mermaid',
+        'datetime': '2024-02-01',
+        'shot': {
+            'EP0001': {
+                'EP0001_0010': {
+                    'fps': 24,
+                    'frange': [1001, 1100],
+                    'author': ['anon', 'aa'],
+                    'filepath': {
+                        'hipfile': '/home/rapa/down',
+                        'nkfile': '/home/rapa/nk',
+                    }
+                }
+            },
+            'EP0002': {
+                'EP002_0010': {
+                    'fps': 24,
+                    'frange': [1001, 1050],
+                    'author': 'anon',
+                }
+            },
+        }
+    }
+}
+def json_modify_easy(j_data, path: list, c_data) -> None:
+    if isinstance(j_data, dict):
+        for k1, v1 in j_data.items():
+            if k1 == path[0]: #
+                if len(path) == 1: # 최종 키값 도달하면 데이터 바꾸기
+                    j_data[k1]= c_data
+                    return
+                json_modify_easy(v1, path[1:], c_data) # 0번 키값 찾으면 그거 빼고 return
+
+    return j_data
+path = []
+chg_data = [1001,1,200]
+a = json_modify_easy(json_data, ['project','shot','EP0002','EP002_0010','author'], chg_data)
+print('1차 >> ')
+pprint.pprint(a)
--- task2/tes2/c2.py
+++ task2/tes2/c2.py
@@ -1,2 +1,28 @@
-print('hlskfjsdlfkjsf')
-pskdfn
+import pprint
+
+json_data = {
+    'project': {
+        'name': 'mermaid',
+        'datetime': '2024-02-01',
+        'shot': {
+            'EP0001': {
+                'EP0001_0010': {
+                    'fps': 24,
+                    'frange': [1001, 1100],
+                    'author': ['anon', 'aa'],
+                    'filepath': {
+                        'hipfile': '/home/rapa/down',
+                        'nkfile': '/home/rapa/nk',
+                    }
+                }
+            },
+            'EP0002': {
+                'EP002_0010': {
+                    'fps': 24,
+                    'frange': [1001, 1050],
+                    'author': 'anon',
+                }
+            },
+        }
+    }
+}
